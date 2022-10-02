2df61cdf6b5e2fc8d03e751760b29a04a3123cb4
Alan Smith <commit@alanwsmith.com>
2022-10-02 15:51:24 -0400

    Removed deprecated code

--- scripts/profile_machine/profile_machine.py
+++ scripts/profile_machine/profile_machine.py
@@ -148,8 +148,6 @@ def run_tests(storehouse):
             with open(storehouse['results_file_path'], 'w') as _results:
                 json.dump(storehouse, _results, sort_keys=True, indent=2, default=str)
 
-
-
 def main():
     log("Starting process")
     machine_details = get_mac_details()
@@ -157,75 +155,16 @@ def main():
         log('TODO: Get EC2 and Windows Names')
         import sys
         sys.exit()
-    storehouse = load_storehouse(machine_details)
-    make_directories(storehouse)
-    download_sample_files(storehouse)
-    output_results(storehouse)
-    run_tests(storehouse)
-    log("Process complete")
+
+    # Make sure you have details loaded before starting
+    if machine_details != None:
+        storehouse = load_storehouse(machine_details)
+        make_directories(storehouse)
+        download_sample_files(storehouse)
+        output_results(storehouse)
+        run_tests(storehouse)
+        log("Process complete")
 
 if __name__ == '__main__':
     main()
-
-import sys
-sys.exit()
-
-
-# Download the test files
-for test_file in data['test_files']:
-
-    # test_file['test_runs'] = []
-    # test_file['file_name'] = test_file['url'].split('/')[-1]
-    # test_file['file_path'] = os.path.join(
-    #     data['test_files_directory'], 
-    #     test_file['file_name']
-    # )
-    # test_file['command'] = [
-    #     data['blender_path'],
-    #     "-b", 
-    #     test_file['file_path'],
-    #     "-noaudio",
-    #     "-o",
-    #     "//render_##", 
-    #     "-E", 
-    #     "CYCLES",
-    #     "-f", 
-    #     "1", 
-    #     "-F",
-    #     "PNG", 
-    #     "-x",
-    #     "1",
-    #     "--", 
-    #     "--cycles-device",
-    #     "METAL"
-    # ]
-    # download_file(
-    #     url=test_file['url'],
-    #     file_path=test_file['file_path']
-    # )
-
-    for test_run in range(1,6):
-        log(f"Doing test run: {test_run}")
-        test_render_path = os.path.join(data['test_files_directory'], 'render_01.png')
-        if os.path.isfile(test_render_path):
-            os.unlink(test_render_path)
-        start_time = datetime.now()
-        subprocess_results = subprocess.run(
-            test_file['command'],
-            capture_output=True,
-            check=True
-        )
-        end_time = datetime.now()
-        time_delta = end_time - start_time
-        test_file['test_runs'].append({
-            'start_time': start_time,
-            'end_time': end_time,
-            'time_delta': time_delta
-        })
-
-        # write the data after each run so you 
-        # don't lose everything if something crashes
-        with open('results.json', 'w') as _results:
-            json.dump(data, _results, sort_keys=True, indent=2, default=str)
-
 
