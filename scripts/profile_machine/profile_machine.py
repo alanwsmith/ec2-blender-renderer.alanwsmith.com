a99e2baf33779301a18d9a246fc2c9380a2797d0
Alan Smith <commit@alanwsmith.com>
2022-10-02 15:07:28 -0400

    Starting to load the config in a storehouse

--- scripts/profile_machine/profile_machine.py
+++ scripts/profile_machine/profile_machine.py
@@ -7,9 +7,31 @@ import os
 
 from datetime import datetime
 from pathlib import Path
+# from pprint import pprint
+
+def download_sample_files(storehouse):
+    for test_file in storehouse['test_files']:
+        print(test_file)
+
+
+    # if not Path(file_path).is_file():
+    #     print(f"Downloading: {url}")
+    #     dir_path = os.path.dirname(file_path)
+    #     Path(dir_path).mkdir(parents=True, exist_ok=True)
+    #     with requests.get(url, stream=True) as response:
+    #         response.raise_for_status()
+    #         tmp_file_path = f'{file_path}.tmp'
+    #         with open(tmp_file_path, 'wb') as f:
+    #             for chunk in response.iter_content(chunk_size=8192):
+    #                 f.write(chunk)
+    #         os.rename(tmp_file_path, file_path)
+    # else:
+    #     print(f"Already downloaded: {url}")
 
 def get_mac_details():
-    mac_details = {}
+    mac_details = {
+        "type": "mac"
+    }
     sysctl_cmd = subprocess.run(
         ['sysctl', 'hw.model'],
         capture_output=True,
@@ -31,27 +53,55 @@ def get_mac_details():
     else:
         return None
 
-# Function to download the sample files
-def download_file(*, url, file_path):
-    if not Path(file_path).is_file():
-        print(f"Downloading: {url}")
-        dir_path = os.path.dirname(file_path)
-        Path(dir_path).mkdir(parents=True, exist_ok=True)
-        with requests.get(url, stream=True) as response:
-            response.raise_for_status()
-            tmp_file_path = f'{file_path}.tmp'
-            with open(tmp_file_path, 'wb') as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    f.write(chunk)
-            os.rename(tmp_file_path, file_path)
-    else:
-        print(f"Already downloaded: {url}")
+def load_storehouse(machine_details):
+    cycles_device = 'CUDA'
+    if machine_details['type'] == 'mac':
+        cycles_device = 'METAL'
+    with open('config.json') as _config_json:
+        storehouse = json.load(_config_json)
+        storehouse['profiler_directory'] = os.path.expanduser(storehouse['profiler_directory'])
+        storehouse['test_files_directory'] = os.path.join(
+            storehouse['profiler_directory'], 'test_files'
+        )
+        for test_file in storehouse['test_files']:
+            test_file['test_runs'] = []
+            test_file['file_name'] = test_file['url'].split('/')[-1]
+            test_file['file_path'] = os.path.join(
+                storehouse['test_files_directory'], 
+                test_file['file_name']
+            )
+            test_file['command'] = [
+                storehouse['blender_path'],
+                "-b", 
+                test_file['file_path'],
+                "-noaudio",
+                "-o",
+                "//render_##", 
+                "-E", 
+                "CYCLES",
+                "-f", 
+                "1", 
+                "-F",
+                "PNG", 
+                "-x",
+                "1",
+                "--", 
+                "--cycles-device",
+                cycles_device
+            ]
+        return storehouse 
 
 def main():
     machine_details = get_mac_details()
     if machine_details == None:
         print('TODO: Get EC2 and Windows Names')
-    print(machine_details)
+        import sys
+        sys.exit()
+    storehouse = load_storehouse(machine_details)
+    print(storehouse)
+    # download_sample_files(storehouse)
+
+
 
 
 if __name__ == '__main__':
@@ -60,20 +110,6 @@ if __name__ == '__main__':
 import sys
 sys.exit()
 
-# Load the config
-with open('config.json') as _config_json:
-    data = json.load(_config_json)
-
-# Expand the home dir `~` to a full path
-data['profiler_directory'] = os.path.expanduser(data['profiler_directory'])
-
-# Set the path to the test files directory
-# (which will get created by the downloader 
-# if necessary)
-data['test_files_directory'] = os.path.join(
-    data['profiler_directory'], 
-    'test_files'
-)
 
 # Download the test files
 for test_file in data['test_files']:
