70583fe8ba7bc797792b3f95d67badd9f6e43fda
Alan Smith <commit@alanwsmith.com>
2022-10-05 19:20:10 -0400

    Added TODO about runnig profiler_

--- scripts/ec2_launcher/reboot-05.bash
+++ scripts/ec2_launcher/reboot-05.bash
@@ -6,6 +6,7 @@ echo "Starting script 05" >> $LOGFILE
 echo "Starting Run" >> $LOGFILE
 cd ~/ec2-blender-renderer.alanwsmith.com/scripts/profile_machine
 python3 profile_machine.py
+echo "TODO: 05- put in profile run here" >> $LOGFILE
 echo "Finished script 05" >> $LOGFILE
 
 
