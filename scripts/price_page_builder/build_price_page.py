f487a27c5a25ce4c38dbb2a2cc417ed3967bf9b7
Alan Smith <commit@alanwsmith.com>
2022-10-07 19:36:51 -0400

    Updates

--- scripts/price_page_builder/build_price_page.py
+++ scripts/price_page_builder/build_price_page.py
@@ -5,12 +5,29 @@ import json
 from string import Template
 
 with open('../report_maker/report_data.json') as _report_data:
+    report_data = json.load(_report_data)
+    machines = []
+    times = []
+    for machine_name in report_data['machines'].keys():
+        print(machine_name)
+        if 'price_base' in report_data['machines'][machine_name]['samples']['loft']['details']:
+            price = "{:.2f}".format(report_data['machines'][machine_name]['samples']['loft']['details']['price_base'])
+            machines.append(f"'{machine_name} - {price}'")
+        else:
+            machines.append(f"'{machine_name} - n/a'")
+        
+        times.append(str(report_data['machines'][machine_name]['samples']['loft']['details']['seconds_average_adjusted']))
+
+
+
+
     with open('template.html') as _tmpl:
         skeleton = Template(_tmpl.read())
         with open('../../site/index.html', 'w') as _html_out:
             _html_out.write(
                 skeleton.substitute(
-                    name="Karl",
+                    LABELS=", ".join(machines),
+                    TIMES=", ".join(times)
                 )
             )
 
