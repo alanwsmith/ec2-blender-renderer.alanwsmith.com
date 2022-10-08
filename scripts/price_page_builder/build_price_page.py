1a8ff7afb39e086ca39aa4fa78548183cc07496a
Alan Smith <commit@alanwsmith.com>
2022-10-08 00:22:31 -0400

    Basic sorting in place

--- scripts/price_page_builder/build_price_page.py
+++ scripts/price_page_builder/build_price_page.py
@@ -6,35 +6,25 @@ from string import Template
 
 with open('../report_maker/report_data.json') as _report_data:
     report_data = json.load(_report_data)
-    machines = []
     machines_v2 = []
     times = []
     for machine_name in report_data['machines'].keys():
         machine = {
-            "name": machine_name.replace('large', '')
+            "name": machine_name.replace('large', '').replace('MacBookPro18.4', 'MBP')
         }
-
         if 'price_base' in report_data['machines'][machine_name]['samples']['loft']['details']:
-            price = "{:.2f}".format(report_data['machines'][machine_name]['samples']['loft']['details']['price_base'])
             machine['price'] = "{:.2f}".format(report_data['machines'][machine_name]['samples']['loft']['details']['price_base'])
-            machines.append(f"'{machine_name} - {price}'")
         else:
             machine['price'] = 'n/a'
-            machines.append(f"'{machine_name} - n/a'")
         
-        # print(machine)
-
-        times.append(str(report_data['machines'][machine_name]['samples']['loft']['details']['seconds_average_adjusted']))
         machine['seconds_average_adjusted'] = report_data['machines'][machine_name]['samples']['loft']['details']['seconds_average_adjusted']
-
         machines_v2.append(machine)
 
     machines_v2.sort(key=lambda x: (x['seconds_average_adjusted'], x['price']))
     print(machines_v2)
 
-    labels = [f"'{item['name']}'" for item in machines_v2]
+    labels = [f"['{item['name']}', '{item['price']}']" for item in machines_v2]
     times = [f"'{item['seconds_average_adjusted']}'" for item in machines_v2]
-    print([item['price'] for item in machines_v2])
 
     with open('template.html') as _tmpl:
         skeleton = Template(_tmpl.read())
@@ -47,6 +37,3 @@ with open('../report_maker/report_data.json') as _report_data:
             )
 
 
-
-#     report_data = json.load(_report_data)
-#     print(report_data)
