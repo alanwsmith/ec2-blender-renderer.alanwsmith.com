fb15f768aac94a796a320f8e01655a760ed51321
Alan Smith <commit@alanwsmith.com>
2022-10-09 01:51:43 -0400

    Filtered down machines

--- scripts/price_page_builder/build_price_page.py
+++ scripts/price_page_builder/build_price_page.py
@@ -4,13 +4,41 @@ import json
 
 from string import Template
 
+exclude_names = [
+    'g3.4xlarge',
+    'g3.8xlarge',
+    'g3.16xlarge',
+    'g3s.xlarge',
+    'g4dn.xlarge',
+    'g4dn.2xlarge',
+    'g4dn.4xlarge',
+    'g4dn.8xlarge',
+    'g4dn.12xlarge',
+    'g4dn.16xlarge',
+    'g5.2xlarge', 
+    'g5.4xlarge', 
+    'g5.8xlarge', 
+    'g5.16xlarge', 
+    'g5.24xlarge', 
+    'p2.xlarge',
+    'p2.8xlarge',
+    'p2.16xlarge',
+    'p3.2xlarge',
+    'p3.8xlarge',
+]
+
+
 with open('../report_maker/report_data.json') as _report_data:
     report_data = json.load(_report_data)
     machines_v2 = []
     times = []
     for machine_name in report_data['machines'].keys():
+        if machine_name in exclude_names:
+            continue
+
         machine = {
-            "name": machine_name.replace('large', '').replace('MacBookPro18.4', 'MBP').replace('windows-', '')
+            #"name": machine_name.replace('large', '').replace('MacBookPro18.4', 'MBP').replace('windows-', '')
+            "name": machine_name.replace('MacBookPro18.4', 'MacBookPro').replace('windows-', 'NVIDIA ')
         }
         if 'price_base' in report_data['machines'][machine_name]['samples']['loft']['details']:
             machine['price'] = "{:.2f}".format(report_data['machines'][machine_name]['samples']['loft']['details']['price_base'])
@@ -24,10 +52,11 @@ with open('../report_maker/report_data.json') as _report_data:
         # multilines
 
         if machine['price'] == 'n/a':
-            machine['id'] = f"{machine['price']}~{machine['name']}"
+            # machine['id'] = f"{machine['price']}~{machine['name']}"
+            machine['id'] = f"{machine['name']}~{machine['price']}"
         else:
-            machine['id'] = f"${machine['price']}~{machine['name']}"
-
+            #machine['id'] = f"${machine['price']}~{machine['name']}"
+            machine['id'] = f"{machine['name']}~${machine['price']}"
 
         machine['label'] = f"{int(machine['seconds'] / 60)}m{machine['seconds'] % 60}s"
         machines_v2.append(machine)
@@ -39,11 +68,20 @@ with open('../report_maker/report_data.json') as _report_data:
         'datasets': [
             {
                 'data': machines_v2,
-                 'backgroundColor': ['#256D85', '#628E90'],
+                 'backgroundColor': [
+                     'green', 
+                     'green',
+                     'green', 
+                     'blue',
+                     'blue'
+                 ],
                  'datalabels': {
-                     'anchor': 'end',
-                     'color': '#000',
-                     'align': 'bottom',
+                    'backgroundColor': '#333',
+                    'anchor': 'end',
+                    'color': '#cc7',
+                    'align': 'bottom',
+                    'font': {
+                    }
                  },
              },
         ],
